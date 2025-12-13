8c3296cee2744acb164dee2387e3f1f09678307f
requiem33 <chutian.tongji@gmail.com>
2025-12-13 17:58:04 +0800

    feat(backend)：创建DjangoTournamentStatus模型

--- backend/apps/fencing_organizer/admin.py
+++ backend/apps/fencing_organizer/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
 from backend.apps.fencing_organizer.modules.fencer.admin import FencerAdmin
+from backend.apps.fencing_organizer.modules.tournament_status.admin import TournamentStatusAdmin
